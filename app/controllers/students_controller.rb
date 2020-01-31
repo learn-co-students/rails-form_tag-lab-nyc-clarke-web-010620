562ebec62b36bb0bede4b5065f8098404183cbf0
github username <github email address>
2020-01-31 17:35:19 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,31 @@
+#require 'pry'
 class StudentsController < ApplicationController
   
   def index
     @students = Student.all
   end
 
-  def show
-    @student = Student.find(params[:id])
+  
+
+  def new
   end
 
+  def create
+    @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    id = @student.id
+    #binding.pry
+    #byebug
+    render :action => "show"
+  end
+
+  def show
+    @student = Student.find(params[:id])
+    byebug
+  end
+
+  private
+  def student_params(*args)
+		params.require(:student).permit(*args)
+	end
 end
